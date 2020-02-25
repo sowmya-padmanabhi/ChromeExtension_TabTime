ce9c11f8741e98583a7b82a17c59d193d2acbae6
sowmya-padmamabhi <sowmya.p.2710@gmail.com>
2020-02-25 02:07:25 -0500

    Fixed index sorting

--- popup.js
+++ popup.js
@@ -14,29 +14,28 @@ chrome.tabs.query({}, function(tabs){
     let tabIds = Object();
     for (var tab in tabs) {
         let key = String(tabs[tab].id);
-        tabIds[tabs[tab].index] = key;
+        tabIds[(tabs[tab].index)+1] = key;
         if (tabs[tab].active) {
             active_key = key;
         }
     }
 
     chrome.storage.sync.get(Object.values(tabIds), function(value) {
-        Object.keys(tabIds).sort().forEach(tabIndex => {
+       Object.keys(tabIds).sort((n1,n2)=>n1-n2).forEach(tabIndex => {
             d = new Date(null);
-            key = tabIds[tabIndex];
+            key = (tabIds[tabIndex]);
             if (value[key]) {
                 let image = value[key].favIconUrl;
-                let url = value[key].url;
                 if (key === active_key) {
                     d.setMilliseconds(value[key].duration + (new Date() - new Date(value[key].start_time)));
                     displayValue = d.toISOString().substr(11,8);
-                    $('#tabs_table ul').append('<li><a href="'+url+'" target="_blank">'+(parseInt(tabIndex)+1)+'  | <image src="'+image+'" height="20"> |  '+'<div id="active_item">'+displayValue+'</div></a></li>')
+                    $('#tabs_table ul').append('<li><a href="#" target="_blank">'+ (parseInt(tabIndex))+'  <image src="'+image+'" height="20"> '+'<div id="active_item" text-align:"right" >'+displayValue+'</div></a></li>')
                     setInterval(addTimer, 1000, value[key]);
 
                 } else {
                     d.setMilliseconds(value[key].duration);
                     displayValue = d.toISOString().substr(11,8);
-                    $('#tabs_table ul').append('<li><a href="'+url+'" target="_blank">'+(parseInt(tabIndex)+1)+'  | <image src="'+image+'" height="20"> |  '+displayValue+'</a></li>')
+                    $('#tabs_table ul').append('<li><a href="#" target="_blank">'+(parseInt(tabIndex))+'  <image src="'+image+'" height="20">'+displayValue+'</a></li>')
                 }
             }
         });
